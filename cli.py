b32da34dc4621d82f17a5d9248111239c2db067e
someone <agrawal.neil434@gmail.com>
2026-03-31 16:59:40 -0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,1 +1,15 @@
+import argparse
+from vfm.core import run_app
 
+def main():
+    parser = argparse.ArgumentParser(description="Visual File Manager")
+
+    parser.add_argument("-y", "--yes", action="store_true", help="Auto confirm prompts")
+    parser.add_argument("-h", "--help", action="help", help="Show help message")
+
+    args = parser.parse_args()
+
+    run_app(args)
+
+if __name__ == "__main__":
+    main()
