4675b6dddec326ec756f1c1fb77c52b498233b61
someone <agrawal.neil434@gmail.com>
2026-03-31 17:04:54 -0700

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,1 +1,17 @@
+import os
 
+def print_tree(path, prefix=""):
+    try:
+        items = os.listdir(path)
+    except PermissionError:
+        print(prefix + "[Permission Denied]")
+        return
+
+    for item in items:
+        full = os.path.join(path, item)
+
+        if os.path.isdir(full):
+            print(prefix + "[D] " + item)
+            print_tree(full, prefix + "  ")
+        else:
+            print(prefix + "[F] " + item)
